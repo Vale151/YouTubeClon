2964d02dabc4417147e63bf2425035a9fdb358ae
Vale151 <user@Soras-MacBook-Pro.local>
2022-06-05 15:32:03 -0300

    add models

---
+++ YouTubeClon/YouTubeClon/Model/PlaylistItemsModel.swift
@@ -0,0 +1,28 @@
+//
+//  PlaylistItemsModel.swift
+//  YouTubeClone
+//
+//  Created by user on 05/06/2022.
+//
+
+import Foundation
+
+struct PlaylistItemsModel: Decodable{
+    let kind: String
+    let etag: String
+    let items: [PlaylistItems]
+    let pageInfo : PageInfo
+    
+    struct PlaylistItems: Decodable{
+        let kind : String
+        let etag : String
+        let id : String
+        let snippet : VideoModel.Item.Snippet
+    }
+    
+    struct PageInfo: Decodable{
+        let totalResults: Int
+        let resultsPerPage: Int
+    }
+}
+
---
+++ YouTubeClon/YouTubeClon/Model/PlaylistModel.swift
@@ -0,0 +1,58 @@
+//
+//  PlaylistModel.swift
+//  YouTubeClon
+//
+//  Created by user on 05/06/2022.
+//
+
+import Foundation
+
+struct PlaylistModel: Decodable {
+    let kind: String
+    let etag: String
+    let pageInfo: PageInfo
+    
+    struct PageInfo: Decodable {
+        let totalResults: Int
+        let resultsPerPage: Int
+    }
+    
+    let items: [Item]
+    
+    struct Item: Decodable {
+        let kind: String
+        let etag: String
+        let id: String
+        let snippet: Snippet
+        let contentDetails: ContentDetails
+        
+        struct Snippet: Decodable {
+            let publishedAt: String
+            let channelId: String
+            let title: String
+            let description: String
+            let thumbnails: Thumbnails
+            let channelTitle: String
+            let localized: Localized
+            
+            struct Thumbnails: Decodable {
+                let medium: Medium
+                
+                struct Medium: Decodable {
+                    let url: String
+                    let width: Int
+                    let heigh: Int
+                }
+            }
+            
+            struct Localized: Decodable {
+                let title: String
+                let description: String
+            }
+        }
+    
+        struct ContentDetails: Decodable {
+            let itemCount: Int
+        }
+    }
+}
